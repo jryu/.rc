e2b5fb9b3612dc623cbd96881200d840d727fa7e
Junho Ryu <jryu@users.noreply.github.com>
2019-05-03 13:36:51 -0700

    install.sh can be executed multiple times.
    Executing install.sh multiple times does not create any side effect.
    Also add some comments.

--- install.sh
+++ install.sh
@@ -3,14 +3,18 @@
 # http://stackoverflow.com/a/246128/796907
 readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-readonly SYMBOLIC_LINK_FILES=('.screenrc' '.vimrc' '.tmux.conf')
+# Create symbolic links
+readonly SYMBOLIC_LINK_FILES=('.screenrc' '.tmux.conf' '.vimrc')
 for f in "${SYMBOLIC_LINK_FILES[@]}"; do ln -v -f -s $DIR/$f ~/$f; done
 
+mkdir -p ~/.vim/ftplugin && ln -v -f -s $DIR/python.vim ~/.vim/ftplugin
+
+# git config changes
 git config --global core.excludesfile ~/.rc/git_ignore_global
 git config --global core.editor "vim"
 
-mkdir -p ~/.vim/ftplugin && ln -f -s $DIR/python.vim ~/.vim/ftplugin
-
-echo '' >> ~/.bashrc
-echo source '~/.rc/bashrc.sh' >> ~/.bashrc
-echo '' >> ~/.bashrc
+# source bashrc.sh
+readonly SOURCE_BASHRC_CMD="source ~/.rc/bashrc.sh"
+if ! grep -q "$SOURCE_BASHRC_CMD" ~/.bashrc; then
+  printf "\n$SOURCE_BASHRC_CMD" >> ~/.bashrc
+fi

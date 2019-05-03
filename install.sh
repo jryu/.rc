78d2d995b64af443a1f7eb5e4679902cef5860e6
Junho Ryu <jryu@users.noreply.github.com>
2019-05-03 12:23:21 -0700

    Add .tmux.conf

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# http://stackoverflow.com/a/246128/796907 
+# http://stackoverflow.com/a/246128/796907
 readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-readonly FILE_PATTERN=.?*rc
-
-# http://stackoverflow.com/a/965072/796907
-for f in $DIR/$FILE_PATTERN; do ln -f -s $f ~/$(basename "$f"); done
-
-ls -la ~/$FILE_PATTERN
+readonly SYMBOLIC_LINK_FILES=('.screenrc' '.vimrc' '.tmux.conf')
+for f in "${SYMBOLIC_LINK_FILES[@]}"; do ln -v -f -s $DIR/$f ~/$f; done
 
 git config --global core.excludesfile ~/.rc/git_ignore_global
 git config --global core.editor "vim"
